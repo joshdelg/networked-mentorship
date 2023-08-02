702854c44310c1093c47bbc442a6fea62bb2ca70
Joshua Delgadillo <joshdelg@stanford.edu>
2023-08-01 23:54:10 -0400

    Testimonials added and basic text changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 
 import Hero from '@/components/hero'
 import Steps from '@/components/steps'
+import Testimonials from '@/components/testimonials'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Testimonials />
       <Steps />
     </main>
   )
---
+++ src/components/testimonials.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Testimonials = () => {
+    return (
+        <section className="p-6 bg-slate-100">
+            <div className="flex flex-row justify-between items-center text-2xl tracking-wide">
+                <div className="mx-6 p-6 bg-slate-200">Bloomberg</div>
+                <div className="mx-6 p-6 bg-slate-200">Bloomberg</div>
+                <div className="mx-6 p-6 bg-slate-200">Bloomberg</div>
+            </div>
+        </section>
+    )
+}
+
+export default Testimonials
